122d356b9d2efc318c0d889bacea57bd6b00720f
Kislov Kirill <kislov-k@inbox.ru>
2023-10-14 09:53:55 +0300

    init proj

---
+++ HttpListenerTeamplate/HttpListenerTeamplate/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using System.Text;
+
+var httpListener = new HttpListener();
+httpListener.Prefixes.Add("http://localhost:5000/");
+
+
+httpListener.Start();
+while (httpListener.IsListening)
+{
+    var context = await httpListener.GetContextAsync();
+    var response = context.Response;
+    var request = context.Request;
+    _ = Task.Run(async () =>
+    {
+        await response.OutputStream.WriteAsync(
+            Encoding.UTF8.GetBytes("Hello World!"));
+        
+        response.OutputStream.Close();
+        response.Close();
+    });
+}
+httpListener.Stop();
+httpListener.Close();
